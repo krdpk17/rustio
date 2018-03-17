0ca6cddae8556507b227b6ee04d10926fd428487
krdpk17 <33544658+krdpk17@users.noreply.github.com>
2018-03-17 14:35:18 +0530

    Update rust_file_create_ifnotfound.rs
    no need of let f in second line.. it avoids warning

--- rust_file_create_ifnotfound.rs
+++ rust_file_create_ifnotfound.rs
@@ -3,7 +3,7 @@ use std::io::ErrorKind;
 
 fn main(){
     let f = File::open("hello.txt");
-    let f = match f {
+    match f {
         Ok(file) => file,
         Err(ref error) if error.kind() == ErrorKind::NotFound => {
             match File::create("hello.txt") {
